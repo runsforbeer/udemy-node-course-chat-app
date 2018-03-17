a174f7ae924d18a40112bdcfbe0020816b89343a
Don Marsh <don@Dons-MacBook-Pro.local>
2018-03-16 23:55:57 -0400

    scroll to bottom on new message

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,22 @@
 var socket = io(); // init socket.io request - open a socket and keep open
 
+function scrollToBottom() {
+    // selectors
+    var message = $('#messages');
+    var newMessage = message.children('li:last-child');
+
+    // heights
+    var clientHeight = message.prop('clientHeight');
+    var scrollTop = message.prop('scrollTop');;
+    var scrollHeight = message.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        message.scrollTop(scrollHeight);
+    }
+}
+
 socket.on('connect', function() {
     console.log('connected to server...');
 });
@@ -9,24 +26,33 @@ socket.on('disconnect', function() {
 });
 
 socket.on('newMessage', function(message) {
-    console.log('newMessage', message);
-
     var formattedTime = moment(message.createdAt).format('h:mm a');
-    var li = jQuery('<li></li>');
-    li.text(`${formattedTime} ${message.from}: ${message.text}`);
 
-    jQuery('#messages').append(li);
+    var template = $('#message-template').html();
+    var html = Mustache.render(template, {
+        text: message.text,
+        from: message.from,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 
 socket.on('newLocationMessage', function(locationMessage) {
     var formattedTime = moment(locationMessage.createdAt).format('h:mm a');
-    var li = $('<li></li>');
-    var a = $('<a target="_blank">My Current Location</a>')
-    li.text(`${formattedTime} ${locationMessage.from}: `);
-    a.attr('href', locationMessage.url);
-    li.append(a);
-    $('#messages').append(li);
+
+    var template = $('#location-message-template').html();
+    var html = Mustache.render(template, {
+        text: 'My current location',
+        from: locationMessage.from,
+        url: locationMessage.url,
+        createdAt: formattedTime
+    });
+
+    $('#messages').append(html);
+    scrollToBottom();
 });
 
 jQuery('#message-form').on('submit', function(e) {
